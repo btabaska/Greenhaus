3761916893d48a4c1a955bdfbf7f3385a64e7f28
Brandon Tabaska <btabaska@gmail.com>
2021-01-19 19:40:59 -0800

    working on routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,1 +1,39 @@
-//TODO ADD HTML ROUTES
+// Dependencies
+const path = require("path");
+const express = require("express");
+const router = express.Router();
+const homeController = require("../controllers/home");
+const uploadController = require("../controllers/upload");
+const upload = require("../middleware/upload");
+
+// Routes
+let routes = (app) => {
+  //Upload Page
+  router.get("/uploads", homeController.getHome);
+  //Home Page
+  router.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+  );
+
+  // user manager route
+  router.get("/users", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/user-manager.html"))
+  );
+
+  // plants route goes to plants.html
+  router.get("/plants", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/plants.html"))
+  );
+
+  // no idea what to do with this route right now.
+  router.get("/users", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/user.html"))
+  );
+  router.post("/upload", upload.single("file"), uploadController.uploadFiles);
+
+  return app.use("/", router);
+};
+
+module.exports = routes;
+
+let routes = (app) => {};
