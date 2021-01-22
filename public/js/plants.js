68d7aecc1b4e126fa3a05286a207592b50271633
Maxx <maxhiga.hawaii@gmail.com>
2021-01-21 19:30:30 -1000

    EDITED: changed all authors and posts to users and plants. changed the js that creates html for each plant. changed title and body for the post to name and note for the plant which is all thats required to make a new plant in database

--- public/js/plants.js
+++ public/js/plants.js
@@ -1,151 +1,151 @@
 //TODO ADD PLANTS JS BASED ON BLOG.JS
 // Wait for the DOM to completely load before we run our JS
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM loaded! 🚀');
-  
-    const plantContainer = document.querySelector('.plant-container');
-  
-    // Variable to hold our posts
-    let plants;
-  
-    const getPlants = (user) => {
-    userId = user || '';
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM loaded! 🚀");
+
+  const plantContainer = document.querySelector(".plant-container");
+
+  // Variable to hold our plants
+  let plants;
+
+  const getPlants = (user) => {
+    userId = user || "";
     if (userId) {
-        userId = `/?user_id=${userId}`;
+      userId = `/?user_id=${userId}`;
     }
-  
+
     fetch(`/api/plants${userId}`, {
-        method: 'GET',
-        headers: {
-        'Content-Type': 'application/json',
-        },
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-        .then((response) => response.json())
-        .then((data) => {
-        posts = data;
-        console.log('Success in getting posts:', data);
+      .then((response) => response.json())
+      .then((data) => {
+        plants = data;
+        console.log("Success in getting plants:", data);
         if (!data || !data.length) {
-            displayEmpty(user);
+          displayEmpty(user);
         } else {
-            initializeRows();
+          initializeRows();
         }
-        })
-        .catch((error) => console.error('Error:', error));
-    };
+      })
+      .catch((error) => console.error("Error:", error));
+  };
 
-    // Get a plant from a specific user
-    const url = window.location.search;
-    let userId;
-    if (url.indexOf('?user_id=') !== -1) {
-    userId = url.split('=')[1];
+  // Get a plant from a specific user
+  const url = window.location.search;
+  let userId;
+  if (url.indexOf("?user_id=") !== -1) {
+    userId = url.split("=")[1];
     getPlants(userId);
-    } else {
+  } else {
     getPlants();
-    }
+  }
 
-    // Front end call to DELETE (kill) a plant
-    const deletePlant = (id) => {
+  // Front end call to DELETE (kill) a plant
+  const deletePlant = (id) => {
     fetch(`/api/plants/${id}`, {
-        method: 'DELETE',
-        headers: {
-        'Content-Type': 'application/json',
-        },
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
     }).then(getPlants());
-    };
-  
-    // Create HTML rows for the blog container **UPDATE: blogContainer name**
-    const initializeRows = () => {
-    plantContainer.innerHTML = '';
+  };
+
+  // Create HTML rows for the blog container **UPDATE: blogContainer name**
+  const initializeRows = () => {
+    plantContainer.innerHTML = "";
     const plantsToAdd = [];
 
     plants.forEach((plant) => plantsToAdd.push(createNewRow(plant)));
     plantsToAdd.forEach((plant) => plantContainer.append(plant));
-    };
+  };
 
-    const createNewRow = (plant) => {
-    console.log('createNewRow -> plant', plant);
+  const createNewRow = (plant) => {
+    console.log("createNewRow -> plant", plant);
 
     const formattedDate = new Date(plant.createdAt).toLocaleDateString();
 
-    const newPostCard = document.createElement('div');
-    newPostCard.classList.add('card');
+    const newPlantCard = document.createElement("div");
+    newPlantCard.classList.add("card");
 
-    const newPostCardHeading = document.createElement('div');
-    newPostCardHeading.classList.add('card-header');
-  
-      // Delete button **UPDATE PostDelete**
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'x';
-    deleteBtn.classList.add('delete', 'btn', 'btn-danger');
-    deleteBtn.addEventListener('click', handlePostDelete);
-  
-      // Edit button
-    const editButton = document.createElement('button');
-    editButton.textContent = 'EDIT';
-    editButton.classList.add('edit', 'btn', 'btn-info');
-    editButton.addEventListener('click', handlePostEdit);
+    const newPlantCardHeading = document.createElement("div");
+    newPlantCardHeading.classList.add("card-header");
 
-    const newPostTitle = document.createElement('h2');
-    const newPostDate = document.createElement('small');
-    const newPostAuthor = document.createElement('h5');
+    // Delete button **UPDATE PostDelete**
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x";
+    deleteBtn.classList.add("delete", "btn", "btn-danger");
+    deleteBtn.addEventListener("click", handlePlantDelete);
 
-    newPostAuthor.textContent = `Written by: ${plant.User.name}`;
-    newPostAuthor.style.float = 'right';
-    newPostAuthor.style.color = 'blue';
-    newPostAuthor.style.marginTop = '-10px';
+    // Edit button
+    const editButton = document.createElement("button");
+    editButton.textContent = "EDIT";
+    editButton.classList.add("edit", "btn", "btn-info");
+    editButton.addEventListener("click", handlePlantEdit);
 
-    const newPostCardBody = document.createElement('div');
-    newPostCardBody.classList.add('card-body');
+    const newPlantName = document.createElement("h2");
+    const newPlantDate = document.createElement("small");
+    const newPlantUser = document.createElement("h5");
 
-    const newPostBody = document.createElement('p');
-    newPostTitle.textContent = `${plant.title} `;
-    newPostBody.textContent = plant.body;
-    newPostDate.textContent = ` (${formattedDate})`;
-    newPostTitle.append(newPostDate);
-    newPostCardHeading.append(deleteBtn);
-    newPostCardHeading.append(editButton);
-    newPostCardHeading.append(newPostTitle);
-    newPostCardHeading.append(newPostAuthor);
-    newPostCardBody.append(newPostBody);
-    newPostCard.append(newPostCardHeading);
-    newPostCard.append(newPostCardBody);
-    newPostCard.setAttribute('data-plant', JSON.stringify(plant));
+    newPlantUser.textContent = `Written by: ${plant.User.name}`;
+    newPlantUser.style.float = "right";
+    newPlantUser.style.color = "blue";
+    newPlantUser.style.marginTop = "-10px";
 
-    console.log('createNewRow -> newPlantCard', newPlantCard);
+    const newPlantCardBody = document.createElement("div");
+    newPlantCardBody.classList.add("card-body");
+
+    const newPlantBody = document.createElement("p");
+    newPlantName.textContent = plant.name;
+    newPlantBody.textContent = plant.note;
+    newPlantDate.textContent = ` (${formattedDate})`;
+    newPlantName.append(newPlantDate);
+    newPlantCardHeading.append(deleteBtn);
+    newPlantCardHeading.append(editButton);
+    newPlantCardHeading.append(newPlantName);
+    newPlantCardHeading.append(newPlantUser);
+    newPlantCardBody.append(newPlantBody);
+    newPlantCard.append(newPlantCardHeading);
+    newPlantCard.append(newPlantCardBody);
+    newPlantCard.setAttribute("data-plant", JSON.stringify(plant));
+
+    console.log("createNewRow -> newPlantCard", newPlantCard);
     return newPlantCard;
-    };
+  };
 
-    // Helper function to display something when there are no posts
-    const displayEmpty = (id) => {
+  // Helper function to display something when there are no plants
+  const displayEmpty = (id) => {
     const query = window.location.search;
-    let partial = '';
+    let partial = "";
     if (id) {
-        partial = ` for User #${id}`;
+      partial = ` for User #${id}`;
     }
     //***Update blogContainer name
-    plantContainer.innerHTML = '';
-    const messageH2 = document.createElement('h2');
-    messageH2.style.textAlign = 'center';
-    messageH2.style.marginTop = '50px';
+    plantContainer.innerHTML = "";
+    const messageH2 = document.createElement("h2");
+    messageH2.style.textAlign = "center";
+    messageH2.style.marginTop = "50px";
     messageH2.innerHTML = `No plants in${partial}, navigate <a href='/cms${query}'>here</a> in order to get your GreenHaus started.`;
     plantContainer.append(messageH2);
-    };
-  
-    // Handle when we click the delete post button
-    const handlePlantDelete = (e) => {
+  };
+
+  // Handle when we click the delete plant button
+  const handlePlantDelete = (e) => {
     const currentPlant = JSON.parse(
-        e.target.parentElement.parentElement.dataset.plant
+      e.target.parentElement.parentElement.dataset.plant
     );
 
-    deletePost(currentPost.id);
-    };
-  
-    // Handle when we click the edit post button
-    const handlePostEdit = (e) => {
+    deletePlant(currentPlant.id);
+  };
+
+  // Handle when we click the edit plant button
+  const handlePlantEdit = (e) => {
     const currentPlant = JSON.parse(
-        e.target.parentElement.parentElement.dataset.plant
+      e.target.parentElement.parentElement.dataset.plant
     );
 
-    window.location.href = `/?plant_id=${currentPost.id}`;
-    };
+    window.location.href = `/?plant_id=${currentPlant.id}`;
+  };
 });
