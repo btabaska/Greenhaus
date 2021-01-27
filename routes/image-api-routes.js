59a5b7bde8c4ad4e6e0dad01e62bb27be28c6da3
Brandon Tabaska <btabaska@gmail.com>
2021-01-26 22:10:11 -0800

    finished images

--- routes/image-api-routes.js
+++ routes/image-api-routes.js
@@ -3,24 +3,25 @@ const db = require("../models");
 
 //routes
 module.exports = (app) => {
-  app.get("/api/images", (req, res) => {
-    const query = {};
-    if (req.query.plant_id) {
-      query.PlantId = req.query.plant_id;
-    }
-    db.Image.findAll({
-      where: query,
-      include: [db.Plant],
-    }).then((dbImage) => res.json(dbImage));
-  });
-
   //get route for retrieving info from a single image
   app.get("/api/images/:id", (req, res) => {
     db.Image.findOne({
       where: {
-        id: req.params.id,
+        PlantId: req.params.id,
       },
       include: [db.Plant],
-    }).then((dbImage) => res.json(dbImage));
+    }).then((dbImage) => res.send(dbImage.dataValues.data));
   });
 };
+
+// const blobToImage = (blob) => {
+//   return new Promise((resolve) => {
+//     const url = URL.createObjectURL(blob);
+//     let img = new Image();
+//     img.onload = () => {
+//       URL.revokeObjectURL(url);
+//       resolve(img);
+//     };
+//     img.src = url;
+//   });
+// };
