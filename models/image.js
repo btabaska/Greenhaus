e4d6b089b84a3d8ce5a5988cf19536fbf1748bd6
James Gilley <jamesgilley1@icloud.com>
2021-01-29 17:30:42 -0600

    updating api

--- models/image.js
+++ models/image.js
@@ -1,26 +1,20 @@
 //exporting database table for photos
 module.exports = (sequelize, DataTypes) => {
-  const Image = sequelize.define("Image", {
-    type: {
-      type: DataTypes.STRING,
-    },
-    name: {
-      type: DataTypes.STRING,
-    },
-    data: {
-      type: DataTypes.BLOB("long"),
-    },
-  });
+  const Image = sequelize.define('Image', {
+    url: {
+      type: DataTypes.STRING
+    }
+  })
 
-  Image.associate = (models) => {
+  Image.associate = models => {
     //We are saying that an Image should belong to a Plant
     //An image can't be created without a Plant due to the foreign key constraint
     Image.belongsTo(models.Plant, {
       foreignkey: {
-        allowNull: false,
-      },
-    });
-  };
+        allowNull: false
+      }
+    })
+  }
 
-  return Image;
-};
+  return Image
+}
--- routes/plant-api-routes.js
+++ routes/plant-api-routes.js
@@ -1,49 +1,60 @@
 //Dependency
-const db = require("../models");
+const axios = require('axios')
+const db = require('../models')
+
+const trefleUrl = `https://trefle.io/api/v1/plants/search?token=hlXm0tZIyyX9DOk8D-9yiTodZaGNOaqqu8d7NP8T8kM&q=`
 
 //routes
-module.exports = (app) => {
-  app.get("/api/plants", (req, res) => {
-    const query = {};
+module.exports = app => {
+  app.get('/api/plants', (req, res) => {
+    const query = {}
     if (req.query.user_id) {
-      query.UserId = req.query.user_id;
+      query.UserId = req.query.user_id
     }
     db.Plant.findAll({
       where: query,
-      include: [db.User],
-    }).then((dbPlant) => res.json(dbPlant));
-  });
+      include: [db.User]
+    }).then(dbPlant => res.json(dbPlant))
+  })
 
   //get route for retrieving info from a single plant
-  app.get("/api/plants/:id", (req, res) => {
+  app.get('/api/plants/:id', (req, res) => {
     db.Plant.findOne({
       where: {
-        id: req.params.id,
+        id: req.params.id
       },
-      include: [db.User],
-    }).then((dbPlant) => res.json(dbPlant));
-  });
+      include: [db.User]
+    }).then(dbPlant => res.json(dbPlant))
+  })
 
   //POST route for saving a new plant
-  app.post("/api/plants", (req, res) => {
-    db.Plant.create(req.body).then((dbPlant) => res.json(dbPlant));
-  });
+  app.post('/api/plants', async (req, res) => {
+    const { name } = req.body
+
+    const resp = await axios.get(trefleUrl + name)
+    const imageUrl = resp.data.data[0].image_url
+    const dbPlant = await db.Plant.create(req.body)
+    console.log({ dbPlant })
+    console.log('dbPlant.id', dbPlant.id)
+    const dbImage = await db.Image.create({ PlantId: dbPlant.id, url: imageUrl })
+    res.json(dbPlant)
+  })
 
   //DELETE route for deleting plants
-  app.delete("/api/plants/:id", (req, res) => {
+  app.delete('/api/plants/:id', (req, res) => {
     db.Plant.destroy({
       where: {
-        id: req.params.id,
-      },
-    }).then((dbPlant) => res.json(dbPlant));
-  });
+        id: req.params.id
+      }
+    }).then(dbPlant => res.json(dbPlant))
+  })
 
   //PUT route for updating plants
-  app.put("/api/plants", (req, res) => {
+  app.put('/api/plants', (req, res) => {
     db.Plant.update(req.body, {
       where: {
-        id: req.body.id,
-      },
-    }).then((dbPlant) => res.json(dbPlant));
-  });
-};
+        id: req.body.id
+      }
+    }).then(dbPlant => res.json(dbPlant))
+  })
+}
